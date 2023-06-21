29b25fdb9d249393ede7773090280d0de914705c
Maxime Frémeaux <maxime.fremeaux2@gmail.com>
2023-06-21 17:56:39 +0200

    add alert for bad credentials in login

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { View, TextInput, Image, StyleSheet, Text, TouchableOpacity, KeyboardAvoidingView, Platform} from "react-native";
+import { View, TextInput, Image, StyleSheet, Text, TouchableOpacity, KeyboardAvoidingView, Platform } from "react-native";
 
 import { useNavigation } from '@react-navigation/native';
 import { AuthContext } from '../navigation';
@@ -15,7 +15,7 @@ const LoginForm = () => {
         setEmail(email);
         const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
         setIsValid(emailPattern.test(email));
-        if (email==""){
+        if (email == "") {
             setIsValid(true);
         }
     };
@@ -29,8 +29,7 @@ const LoginForm = () => {
     const handleLoginPress = async () => {
         if (!isValid) {
             alert("Please enter a valid email address.");
-        }
-        else {
+        } else {
             if (email !== "" && password !== "") {
                 const response = await fetch('https://whereisthepubcrawl.com/API/login.php', {
                     method: 'POST',
@@ -38,8 +37,8 @@ const LoginForm = () => {
                         'Content-Type': 'application/json',
                     },
                     body: JSON.stringify({ // pass the email and password from form to the API
-                        email: email, // 'superadmin@gmail.com'
-                        password: password, // '12345678'
+                        email: email,
+                        password: password,
                     }),
                 });
                 const dataRes = await response.json();
@@ -53,13 +52,14 @@ const LoginForm = () => {
                         agentCityId: dataRes.data.agent_city_id,
                     });
                 } else {
+                    alert(dataRes.message);
                     setHasUser(false);
                 }
             } else {
                 alert("Please fill in all fields");
             }
         }
-      };
+    };
 
 
     return (
@@ -68,7 +68,7 @@ const LoginForm = () => {
             keyboardVerticalOffset={0}>
             <View style={styles.view}>
                 <Image style={{ alignSelf: "center", marginBottom: 20 }}
-                source={require('../assets/logo.webp')} />
+                    source={require('../assets/logo.webp')} />
                 <TextInput style={styles.textInput}
                     placeholder="Email"
                     placeholderTextColor={"white"}
@@ -107,58 +107,58 @@ const LoginForm = () => {
         </KeyboardAvoidingView>
     );
 }
-    const styles = StyleSheet.create({
-        textInput: {
-            height: 60,
-            width: 300,
-            borderRadius: 30,
-            borderWidth: 1,
-            backgroundColor: "#f48024",
-            color: "black",
-            paddingLeft: 30,
-            marginBottom: 25,
-        },
-        button: {
-            width: 180,
-            backgroundColor: "#f48024",
-            marginTop: 15,
-            borderWidth: 1,
-            borderRadius: 30,
-            shadowColor: "grey",
-            shadowOpacity: 0.8,
-            shadowOffset: { width: 0, height: 2 },
-            shadowRadius: 4,
-            
-        },
-        buttonText: {
-            color: "black",
-            fontWeight: "bold",
-            textAlign: "center",
-            paddingVertical: 15,
-            fontSize: 18,
-        },
-        registerText: {
-            color: "#f48024",
-            fontWeight: "bold",
-            textAlign: "center",
-            paddingVertical: 15,
-            fontSize: 12,
-        },
-        underline: {
-            textDecorationLine: 'underline',
-        },
-        view: {
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-        },
-        errorText: {
-            color: "red",
-            marginBottom: 15,
-            marginTop: -25,
-            fontSize: 12,
-        }
+const styles = StyleSheet.create({
+    textInput: {
+        height: 60,
+        width: 300,
+        borderRadius: 30,
+        borderWidth: 1,
+        backgroundColor: "#f48024",
+        color: "black",
+        paddingLeft: 30,
+        marginBottom: 25,
+    },
+    button: {
+        width: 180,
+        backgroundColor: "#f48024",
+        marginTop: 15,
+        borderWidth: 1,
+        borderRadius: 30,
+        shadowColor: "grey",
+        shadowOpacity: 0.8,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 4,
+
+    },
+    buttonText: {
+        color: "black",
+        fontWeight: "bold",
+        textAlign: "center",
+        paddingVertical: 15,
+        fontSize: 18,
+    },
+    registerText: {
+        color: "#f48024",
+        fontWeight: "bold",
+        textAlign: "center",
+        paddingVertical: 15,
+        fontSize: 12,
+    },
+    underline: {
+        textDecorationLine: 'underline',
+    },
+    view: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    errorText: {
+        color: "red",
+        marginBottom: 15,
+        marginTop: -25,
+        fontSize: 12,
     }
+}
 );
 
 export default LoginForm;
